00dff5ffa23bdf50755cae5a37d07d9df2991e5d
kvanadev <kvanadev1@gmail.com>
2017-04-14 10:46:50 +0530

    first commit

---
+++ swiftyjson/RestApiManager.swift
@@ -0,0 +1,11 @@
+//
+//  RestApiManager.swift
+//  swiftyjson
+//
+//  Created by kvanadev5 on 19/01/17.
+//  Copyright © 2017 kvanadev5. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ swiftyjson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftyjson
+//
+//  Created by kvanadev5 on 19/01/17.
+//  Copyright © 2017 kvanadev5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
